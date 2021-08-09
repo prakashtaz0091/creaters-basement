61849ebb7f1af36c4652c90c59de3dacb8607a57
prakashtaz0091 <code.ing2468@gmail.com>
2021-08-09 22:15:08 +0545

    procfile

--- project/settings.py
+++ project/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
