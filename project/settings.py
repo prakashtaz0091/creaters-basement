556573ea92da23e57a025de15596c800ce886e4a
prakashtaz0091 <86336006+prakashtaz0091@users.noreply.github.com>
2022-03-26 07:09:49 +0545

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'indoc.0021@gmail.com'
-EMAIL_HOST_PASSWORD = 'gmail@google.com'
+EMAIL_HOST_PASSWORD = ''
